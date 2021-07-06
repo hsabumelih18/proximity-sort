81bcbd8301ac91f9282172ede9d34af80d4ba5a1
asslavov18 <asslavov18@codingburgas.bg>
2021-07-06 12:01:54 +0300

    Implement hammingDistance()

--- proximity-sort/proximity-sort/Program.cs
+++ proximity-sort/proximity-sort/Program.cs
@@ -8,8 +8,28 @@ namespace proximity_sort
 {
     class Program
     {
+        //returns the amount of different characters between two strings with the same size
+        static int hammingDistance(string wordA, string wordB)
+        {
+            //int similarities =Math.Abs( wordA.Length - wordB.Length);
+            int similarities = 0;
+            for (int i = 0; i < wordA.Length; i++)
+            {
+                if (wordA[i] != wordB[i]) similarities++;
+            }
+
+            return similarities;
+        }
+
+
+
         static void Main(string[] args)
         {
+            string a = "kitten";
+            string b = "miffen";
+
+            Console.WriteLine(hammingDistance(a, b));
         }
+
     }
 }
