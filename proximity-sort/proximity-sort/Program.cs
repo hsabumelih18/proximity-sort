4eaef7f0e8cb029f9fbe3a70c904457b2619e6aa
hsabumelih18 <hsabumelih18@codingburgas.bg>
2021-07-13 18:26:09 +0300

    Implement jaroWinkler()

--- proximity-sort/proximity-sort/Program.cs
+++ proximity-sort/proximity-sort/Program.cs
@@ -103,28 +103,99 @@ namespace proximity_sort
         {
             Dictionary<char, int> letterCount1 = getLetters(s1);
             Dictionary<char, int> letterCount2 = getLetters(s2);
-            int matchingChar=0;
+            int matchingChar = 0;
             Dictionary<char, int>.KeyCollection Keys1 = letterCount1.Keys;
 
-            Console.WriteLine();
             foreach (char key in Keys1)
             {
                 if (letterCount2.ContainsKey(key))
                 {
                     // adds the lesser amount of times a character has been seen in both words
-                    matchingChar+= Math.Min(letterCount1[key], letterCount2[key]);
+                    matchingChar += Math.Min(letterCount1[key], letterCount2[key]);
                 }
             }
             return matchingChar;
         }
 
+        static float jaroWinkler(string wordA, string wordB)
+        {
+            float result = 0;
+            int sz1 = wordA.Length;
+            int sz2 = wordA.Length;
+            int matchChar = getSameLettersCount(wordA, wordB);
+            Dictionary<char, int> letterCount1 = getLetters(wordA);
+            Dictionary<char, int> letterCount2 = getLetters(wordB);
+            Dictionary<char, int>.KeyCollection Keys1 = letterCount1.Keys;
+            Dictionary<char, int>.KeyCollection Keys2 = letterCount2.Keys;
+            char[] sameLetters1 = new char[25];
+            char[] sameLetters2 = new char[25];
+
+            char[] matchingLetters2 = new char[25];
+            int count = 0, sameCount;
+            foreach (char key in Keys1)
+            {
+                if (letterCount2.ContainsKey(key))
+                {
+                    sameLetters1[count++] = key;
+                }
+            }
+
+            sameCount = 0;
+            foreach (char key in Keys2)
+            {
+                if (letterCount1.ContainsKey(key))
+                {
+                    sameLetters2[sameCount++] = key;
+                }
+            }
+            count = 0;
+            foreach (char letter in wordB)
+            {
+                foreach (char sameLetter in sameLetters2)
+                {
+                    if (letter == sameLetter)
+                    {
+                        if (letterCount2[letter] > 0)
+                        {
+                            //Console.WriteLine(/*letterCount1[letter] + " " + */letter);
+                            matchingLetters2[count++] = letter;
+                            letterCount2[letter]--;
+
+                        }
+
+                    }
+                }
+
+            }
+            for (int i = 0; i < sameLetters1.Length; i++)
+            {
+                Console.WriteLine(sameLetters1[i]);
+            }
+
+            int nonMatchChar = 0, transpositions;
+            for (int i = 0; i < sameCount; i++)
+            {
+                Console.WriteLine(matchingLetters2[i] + " " + sameLetters1[i]);
+                if (matchingLetters2[i] != sameLetters1[i]) nonMatchChar++;
+            }
+            transpositions = nonMatchChar / 2;
+            Console.WriteLine(matchChar + " " + nonMatchChar + " " + transpositions);
+
+            result = ((float)matchChar / (float)Math.Abs(sz1) + (float)matchChar / (float)Math.Abs(sz1)
+                + ((float)matchChar - (float)transpositions) / (float)matchChar) / (float)3;
+
+            return result;
+        }
+
+
         static void Main(string[] args)
         {
-            string a = "mamka";
-            string b = "amkan";
+            string a = "winkler";
+            string b = "welfare";
 
 
-            Console.WriteLine(getSameLettersCount(a, b));
+            Console.WriteLine(jaroWinkler(a, b));
+            //Console.WriteLine(getSameLettersCount(a, b));
             //Console.WriteLine(hammingDistance(a, b));
             //levenshteinDistance(a, b);
             //Dictionary<char, int> letterCount = getLetters(a);
